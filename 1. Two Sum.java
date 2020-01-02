2ffd94e4c3ac1e23b18513932f66324714a0c09a
Alfred Shi <memoriedsea@gmail.com>
2020-01-02 23:14:39 +0800

    new start!

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        
+
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
                 return new int[] {map.get(target - nums[i]), i};
@@ -9,7 +9,7 @@ class Solution {
                 map.put(nums[i], i);
             }
         }
-        
-        throw new IllegalArgumentException("No two sum solution");
+
+        return null;
     }
 }
