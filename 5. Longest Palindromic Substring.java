5971988a488561630a265b25801757622111c209
Alfred Shi <memoriedsea@gmail.com>
2020-08-22 21:38:18 +0800

    add solutions

--- 5. Longest Palindromic Substring.java
+++ 5. Longest Palindromic Substring.java
@@ -1,52 +1,38 @@
 class Solution {
-    int maxLength = 1;
-    int maxLeftIndex = 0;
-    int maxRightIndex = 0;
     public String longestPalindrome(String s) {
-        int maxLength = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            scanPalindrome(s, i);
-        }
-        
-        return s.substring(maxLeftIndex, maxRightIndex + 1);
-    }
-    
-    public void scanPalindrome(String s, int index) {
-        int maxOdd = 1;
-        int maxEven = 0;
-        
-        if (index + 1 < s.length() && s.charAt(index) == s.charAt(index + 1)) {
-            maxEven = 2;
-            int i = 1;
-            for (; index - i >= 0 && index + 1 + i < s.length(); i++) {
-                if (s.charAt(index - i) == s.charAt(index + 1 + i)) {
-                    maxEven += 2;
-                } else {
-                    break;
+        if (s == null || s.length() == 0) return "";
+
+        int longestLength = 1;
+        String result = s.substring(0, 1);
+
+        char[] array = s.toCharArray();
+
+        for (int i = 0; i < array.length; i++) {
+            int index = 0;
+
+            while (i - index >= 0 && i + index < array.length && array[i - index] == array[i + index]) {
+                if (2 * index + 1 > longestLength) {
+                    longestLength = 2 * index + 1;
+                    result = s.substring(i - index, i + index +1);
+                }
+
+                index++;
+            }
+
+            if (i + 1 < array.length && array[i] == array[i + 1]) {
+                index = 0;
+
+                while (i - index >= 0 && i + 1 + index < array.length && array[i - index] == array[i + index + 1]) {
+                    if (2 * index + 2 > longestLength) {
+                        longestLength = 2 * index + 2;
+                        result = s.substring(i - index, i + 2 + index);
+                    }
+
+                    index++;
                 }
             }
-            
-            if (maxEven > maxLength) {
-                maxLength = maxEven;
-                maxLeftIndex = index - i + 1;
-                maxRightIndex = index + i;
-            }
-        } 
-        
-        int i = 1;
-        for (; index - i >= 0 && index + i < s.length(); i++) {
-            if (s.charAt(index - i) == s.charAt(index + i)) {
-                maxOdd += 2;
-            } else {
-                break;
-            }
         }
-        
-        if (maxOdd > maxLength) {
-            maxLength = maxOdd;
-            maxLeftIndex = index - i + 1;
-            maxRightIndex = index + i - 1;
-        }
+
+        return result;
     }
 }
