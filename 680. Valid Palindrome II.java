99e254eaba39c324bfcb8538f3158e7276f4342f
Alfred Shi <memoriedsea@gmail.com>
2020-08-27 16:53:41 +0800

    add solutions

--- 680. Valid Palindrome II.java
+++ 680. Valid Palindrome II.java
@@ -1,25 +1,25 @@
 class Solution {
-    public boolean validPalindromeFull(char[] cArray, int startIndex, int endIndex) {
-        while (startIndex < endIndex) {
-            if (cArray[startIndex] != cArray[endIndex]) {
+    private boolean hit;
+
+    public boolean validPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+
+        hit = false;
+        
+        return validPalindrome(s, 0, s.length() - 1);
+    }
+
+    public boolean validPalindrome(String s, int startIndex, int endIndex) {
+        if (startIndex > endIndex) return true;
+
+        if (s.charAt(startIndex) == s.charAt(endIndex)) return validPalindrome(s, startIndex + 1, endIndex - 1);
+        else {
+            if (hit) {
                 return false;
             } else {
-                startIndex++;
-                endIndex--;
-            }
-        } 
-        return true;
-    }
-    
-    public boolean validPalindrome(String s) {
-        char[] cArray = s.toCharArray();
-        
-        for (int i = 0; i < s.length() / 2; i++) {
-            if (cArray[i] != cArray[s.length() - 1 - i]) {
-                return validPalindromeFull(cArray, i, s.length() - i - 2) || validPalindromeFull(cArray, i + 1, s.length() - i - 1);
+                hit = true;
+                return validPalindrome(s, startIndex, endIndex - 1) || validPalindrome(s, startIndex + 1, endIndex);
             }
         }
-        
-        return true;
     }
 }
