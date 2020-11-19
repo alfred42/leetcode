12b42d6f575971f4dd3b9880e2f86b67229f727f
Alfred Shi <memoriedsea@gmail.com>
2020-11-19 22:27:00 +0800

    update logic

--- 283. Move Zeroes.java
+++ 283. Move Zeroes.java
@@ -1,17 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int leftIndex = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[leftIndex] = nums[i];
-                leftIndex++;
+        int rightIndex = 0;
+
+        while (rightIndex < nums.length) {
+            if (nums[rightIndex] != 0) {
+                nums[leftIndex++] = nums[rightIndex++];
+            } else {
+                rightIndex++;
             }
         }
-        
+
         while (leftIndex < nums.length) {
-            nums[leftIndex] = 0;
-            leftIndex++;
+            nums[leftIndex++] = 0;
         }
     }
 }
