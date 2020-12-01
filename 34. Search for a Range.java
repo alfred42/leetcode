6a27881b611933a75f6d99bec27a11268caed788
Alfred Shi <memoriedsea@gmail.com>
2020-12-01 23:47:40 +0800

    update solution

--- 34. Search for a Range.java
+++ 34. Search for a Range.java
@@ -1,30 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) return new int[] {-1, -1}; 
+        if (nums == null || nums.length == 0) return new int[] {-1, -1};
+
         int left = 0;
         int right = nums.length - 1;
-        
+
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] < target) left = mid + 1;
-            else if (nums[mid] >= target) right = mid;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
 
-        if (nums[left] != target) return new int[] {-1, -1};
-        
-        int answer0 = left;
-        
+        int leftIndex = nums[left] == target ? left: -1;
+
+        if (leftIndex == -1) return new int[] {-1, -1};
+
+        left = 0;
         right = nums.length - 1;
-        
+
         while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] <= target) left = mid + 1;
-            else if (nums[mid] > target) right = mid - 1;
+            int mid = left + (right - left + 1) / 2;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                left = mid;
+            } else {
+                left = mid + 1;
+            }
         }
         
-        int answer1 = nums[left] == target? left: left - 1;
-        
-        return new int[] {answer0, answer1};
+
+        return new int[] {leftIndex, right};
     }
 }
