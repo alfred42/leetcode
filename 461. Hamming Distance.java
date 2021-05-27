beefa7566ee2fd2a7eb939aaf01c973a1fe3ce40
Alfred Shi <memoriedsea@gmail.com>
2021-05-27 16:03:42 +0800

    update #461

--- 461. Hamming Distance.java
+++ 461. Hamming Distance.java
@@ -1,5 +1,13 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        return Integer.bitCount(x ^ y);
+        int s = x ^ y;
+        int sum = 0;
+
+        while (s != 0) {
+            sum++;
+            s &= s - 1;
+        }
+
+        return sum;
     }
 }
