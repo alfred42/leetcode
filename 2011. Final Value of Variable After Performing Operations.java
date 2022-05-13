7f7575742e2b6afcd922051b43127f89073020c9
Alfred Shi <memoriedsea@gmail.com>
2022-05-13 23:06:09 +0800

    add #2011

---
+++ 2011. Final Value of Variable After Performing Operations.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+
+        for (String operation : operations) {
+            if (operation.contains("++")) {
+                result++;
+            } else {
+                result--;
+            }
+        }
+
+        return result;
+    }
+}
