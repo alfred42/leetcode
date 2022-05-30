9a03626a68d717da2968807b5a7656312ea06c2c
Alfred Shi <memoriedsea@gmail.com>
2022-05-30 23:16:23 +0800

    update #1002

--- 1002. Find Common Characters.java
+++ 1002. Find Common Characters.java
@@ -1,32 +1,30 @@
 class Solution {
-    public List<String> commonChars(String[] A) {
-        int[] buckets = new int[26];
+    public List<String> commonChars(String[] words) {
+        int[] minCounts = new int[26];
 
-        for (int i = 0; i < buckets.length; i++) {
-            buckets[i] = Integer.MAX_VALUE;
-        }        
+        Arrays.fill(minCounts, Integer.MAX_VALUE);
 
-        for (String a : A) {
-            int[] hitBuckets = new int[26];
+        for (String word : words) {
+            int[] counts = new int[26];
 
-            for (char c : a.toCharArray()) {
-                hitBuckets[c - 'a']++;
+            for (char c : word.toCharArray()) {
+                counts[c - 'a']++;
             }
 
-            for (int i = 0; i < buckets.length; i++) {
-                buckets[i] = Math.min(buckets[i], hitBuckets[i]);
+            for (int i = 0; i < counts.length; i++) {
+                minCounts[i] = Math.min(minCounts[i], counts[i]);
             }
         }
 
-        List<String> list = new ArrayList<>();
+        List<String> resultList = new ArrayList<>();
 
-        for (int i = 0; i < buckets.length; i++) {
-            while (buckets[i] > 0) {
-                list.add(String.valueOf((char)('a' + i)));
-                buckets[i]--;
+        for (int i = 0; i < minCounts.length; i++) {
+            while (minCounts[i] > 0) {
+                resultList.add(Character.toString((char)('a' + i)));
+                minCounts[i]--;
             }
         }
 
-        return list;
+        return resultList;
     }
 }
