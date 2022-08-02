89d423327230e61f6b8b3bf7fae7b4cdfda235ab
Alfred Shi <memoriedsea@gmail.com>
2022-08-02 15:05:49 +0800

    add solutions

--- 622. Design Circular Queue.java
+++ 622. Design Circular Queue.java
@@ -1,59 +1,47 @@
 class MyCircularQueue {
-    int[] array;
-    int head;
-    int rear;
-    int size;
+    private int[] values;
+    private int startIndex;
+    private int size;
 
-    /** Initialize your data structure here. Set the size of the queue to be k. */
     public MyCircularQueue(int k) {
-        this.array = new int[k];
-        head = 0;
-        rear = 0;
+        values = new int[k];
+        size = 0;
+        startIndex = 0;
     }
     
-    /** Insert an element into the circular queue. Return true if the operation is successful. */
     public boolean enQueue(int value) {
-        if (size == array.length) return false;
+        if (size == values.length) return false;
 
-        array[rear % array.length] = value;
-        rear++;
-        rear %= array.length;
-
+        values[(startIndex + size) % values.length] = value;
         size++;
         return true;
     }
     
-    /** Delete an element from the circular queue. Return true if the operation is successful. */
     public boolean deQueue() {
         if (size == 0) return false;
 
-        int temp = array[head % array.length];
-        head++;
-        head %= array.length;
-        size--;
+        startIndex = (startIndex + 1) % values.length;
 
+        size--;
         return true;
     }
     
-    /** Get the front item from the queue. */
     public int Front() {
-        return size == 0 ? -1 : array[head];
-
+        if (size == 0) return -1;
+        return values[startIndex];
     }
     
-    /** Get the last item from the queue. */
     public int Rear() {
-        return size == 0 ? -1 : rear == 0 ? array[array.length - 1] : array[rear - 1];
+        if (size == 0) return -1;
+        return values[(startIndex + size - 1) % values.length];
     }
     
-    /** Checks whether the circular queue is empty or not. */
     public boolean isEmpty() {
         return size == 0;
     }
     
-    /** Checks whether the circular queue is full or not. */
     public boolean isFull() {
-        return size == array.length;
+        return size == values.length;
     }
 }
 
