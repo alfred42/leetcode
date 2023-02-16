db57130ee1bd9eb10d05715ce411a4658d88d5f7
Alfred Shi <memoriedsea@gmail.com>
2023-02-16 16:24:15 +0800

    add #2341

--- 2341. Maximum Number of Pairs in Array.java
+++ 2341. Maximum Number of Pairs in Array.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] numberOfPairs(int[] nums) {
         int[] counter = new int[101];
-        
+
         for (int num : nums) {
             counter[num]++;
         }
-        
+
         int pairs = 0;
-        int remainCount = 0;
+        int left = 0;
+
         for (int c : counter) {
             pairs += c / 2;
-            remainCount += c % 2 == 0 ? 0 : 1;
+            left += c % 2;
         }
-        
-        return new int[] {pairs, remainCount};
+
+        return new int[] {pairs, left};
     }
 }
