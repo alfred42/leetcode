ef78be884a9b65ef92461c2923d66cd3c2b59687
Alfred Shi <memoriedsea@gmail.com>
2025-01-06 22:15:06 +0800

    update #700

--- 700. Search in a Binary Search Tree.java
+++ 700. Search in a Binary Search Tree.java
@@ -4,15 +4,24 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
+    
         if (root == null) return null;
 
-        if (root.val == val) return root;
+        if (root.val == val) {
+            return root;
+        }
 
-        return val > root.val ? searchBST(root.right, val) : searchBST(root.left, val);
+       return val > root.val ? searchBST(root.right, val) : searchBST(root.left, val);
     }
 }
