061fc04fef36db1442c37e90881cc1b1de783a25
Alfred Shi <memoriedsea@gmail.com>
2025-01-07 09:55:20 +0800

    add #1550

--- 1550. Three Consecutive Odds.java
+++ 1550. Three Consecutive Odds.java
@@ -2,12 +2,13 @@ class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         int count = 0;
 
-        for (int i : arr) {
-            if (i % 2 != 0) {
+        for (int num : arr) {
+            if (num % 2 == 1) {
                 count++;
             } else {
                 count = 0;
             }
+
             if (count == 3) return true;
         }
 
