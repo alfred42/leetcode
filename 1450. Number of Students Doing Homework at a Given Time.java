30488c9ee4f3b7c3b5243220577b6bb7f2a447d5
Alfred Shi <memoriedsea@gmail.com>
2025-01-11 23:14:04 +0800

    update #1450

--- 1450. Number of Students Doing Homework at a Given Time.java
+++ 1450. Number of Students Doing Homework at a Given Time.java
@@ -1,13 +1,11 @@
 class Solution {
     public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
-        int count = 0;
+        int answer = 0;
 
         for (int i = 0; i < startTime.length; i++) {
-            if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
-                count++;
-            }
+            if (queryTime >= startTime[i] && queryTime <= endTime[i]) answer++;
         }
 
-        return count;
+        return answer;
     }
 }
