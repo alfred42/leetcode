2f6e915eb08ac67cdea96d558f2dbd4c5c25197d
Alfred Shi <memoriedsea@gmail.com>
2025-02-04 21:38:59 +0800

    add #922

--- 922. Sort Array By Parity II.java
+++ 922. Sort Array By Parity II.java
@@ -1,19 +1,28 @@
 class Solution {
-    public int[] sortArrayByParityII(int[] A) {
-        int oddIndex = 0; 
-        int evenIndex = 1;
+    public int[] sortArrayByParityII(int[] nums) {
+        int odd = 0;
+        int even = 1;
 
-        while (oddIndex < A.length && evenIndex < A.length) {
-            while (oddIndex < A.length && A[oddIndex] % 2 == 0) oddIndex += 2;
-            while (evenIndex < A.length && A[evenIndex] % 2 == 1) evenIndex += 2;
-
-            if (oddIndex < A.length && evenIndex < A.length) {
-                int temp = A[oddIndex];
-                A[oddIndex] = A[evenIndex];
-                A[evenIndex] = temp;
+        while (odd < nums.length) {
+            if (nums[odd] % 2 == 0) {
+                odd += 2;
+            } else {
+                while (nums[even] % 2 == 1) {
+                    even += 2;
+                }
+                
+                swap(nums, odd, even);
+                odd += 2;
+                even += 2;
             }
         }
 
-        return A;
+        return nums;
+    }
+
+    private void swap(int[] nums, int leftIndex, int rightIndex) {
+        int temp = nums[leftIndex];
+        nums[leftIndex] = nums[rightIndex];
+        nums[rightIndex] = temp;
     }
 }
