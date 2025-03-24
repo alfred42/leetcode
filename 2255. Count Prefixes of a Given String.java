a2e0275fd9e2b5f443de523b4a38212fc3efb9cb
Alfred Shi <memoriedsea@gmail.com>
2025-03-24 13:32:12 +0800

    add #2255

---
+++ 2255. Count Prefixes of a Given String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countPrefixes(String[] words, String s) {
+        int answer = 0;
+
+        for (String word : words) {
+            if (s.startsWith(word)) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
