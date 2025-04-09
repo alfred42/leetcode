112b689281fd67d501492e552fa7d6efa236cb72
Alfred Shi <memoriedsea@gmail.com>
2025-04-09 08:58:32 +0800

    update #3396

--- 3396. Minimum Number of Operations to Make Elements in Array Distinct.java
+++ 3396. Minimum Number of Operations to Make Elements in Array Distinct.java
@@ -1,42 +1,12 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-            if (!set.contains(num) && map.get(num) > 1) {
-                set.add(num);
+        boolean[] seen = new boolean[128];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (seen[nums[i]]) {
+                return i / 3 + 1;
             }
+            seen[nums[i]] = true;
         }
-
-        if (set.size() == 0) return 0;
-
-        int count = 0;
-
-        for (int i = 0; i < nums.length;) {
-            count++;
-
-            if (i < nums.length) {
-                map.put(nums[i], map.get(nums[i]) - 1);
-                if (map.get(nums[i]) == 1) set.remove(nums[i]);
-            }
-
-            if  (i + 1 < nums.length) {
-                map.put(nums[i + 1], map.get(nums[i + 1]) - 1);
-                if (map.get(nums[i + 1]) == 1) set.remove(nums[i + 1]);
-            }
-            
-            if (i + 2 < nums.length) {
-                map.put(nums[i + 2], map.get(nums[i + 2]) - 1);
-                if (map.get(nums[i + 2]) == 1) set.remove(nums[i + 2]);
-            }
-
-            i += 3;
-
-            if (set.size() == 0) return count;
-        }
-
-        return count;
+        return 0;
     }
 }
