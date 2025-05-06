02459da62c17348a10c7b37af101084aa50ed2be
Alfred Shi <memoriedsea@gmail.com>
2025-05-06 10:50:58 +0800

    add #1920

--- 1920. Build Array from Permutation.java
+++ 1920. Build Array from Permutation.java
@@ -1,13 +1,11 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        if (nums == null) return null;
-        
-        int[] ans = new int[nums.length];
+        int[] answer = new int[nums.length];
 
         for (int i = 0; i < nums.length; i++) {
-            ans[i] = nums[nums[i]];
+            answer[i] = nums[nums[i]];
         }
 
-        return ans;
+        return answer;
     }
 }
