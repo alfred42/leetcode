71a1d36eeedb885aa74bbc25229aa7b180887b2b
Alfred Shi <memoriedsea@gmail.com>
2026-01-11 15:25:52 +0800

    refactor solution for 85. Maximal Rectangle to use a more efficient approach

--- 85. Maximal Rectangle.java
+++ 85. Maximal Rectangle.java
@@ -1,22 +1,31 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int[] temp = new int[matrix[0].length];
 
         int maxArea = 0;
-        int[][] dp = new int[matrix.length][matrix[0].length];
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == '1') {
-                    dp[i][j] = j == 0 ? 1 : dp[i][j - 1] + 1;
-
-                    int width = dp[i][j];
-                    
-                    for (int k = i; k >= 0; k--) {
-                        width = Math.min(width, dp[k][j]);
-                        maxArea = Math.max(maxArea, width * (i - k + 1));
-                    }
+                    temp[j]++;
+                } else {
+                    temp[j] = 0;
                 }
+            }
+
+            for (int j = 0; j < matrix[0].length; j++) {
+                int left = j;
+                int right = j;
+
+                while (left >= 0 && temp[left] >= temp[j]) {
+                    left--;
+                }
+
+                while (right < matrix[0].length && temp[right] >= temp[j]) {
+                    right++;
+                }
+
+                maxArea = Math.max(maxArea, temp[j] * (right - left - 1));
             }
         }
 
