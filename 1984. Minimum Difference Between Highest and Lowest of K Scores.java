09578c7613235aba5bdaa6dfd4fc1798726f2102
Alfred Shi <memoriedsea@gmail.com>
2026-01-25 22:39:09 +0800

    add solution for 1984. Minimum Difference Between Highest and Lowest of K Scores

---
+++ 1984. Minimum Difference Between Highest and Lowest of K Scores.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int minValue = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length - k +1; i++) {
+            minValue = Math.min(minValue, nums[i + k - 1] - nums[i]);
+        }
+
+        return minValue;
+    }
+}
